bdeb0e333d7bf960fe403ecb4ba54a65ae8cfcb4
Julian Festing <julfesting@gmail.com>
2023-05-05 11:08:10 +0100

    changes to database connection for depol

--- music_library_database_app/lib/database_connection.rb
+++ music_library_database_app/lib/database_connection.rb
@@ -1,5 +1,3 @@
-# file: lib/database_connection.rb
-
 require 'pg'
 
 # This class is a thin "wrapper" around the
@@ -11,23 +9,18 @@ class DatabaseConnection
   # PG gem. We connect to 127.0.0.1, and select
   # the database name given in argument.
   def self.connect
+    # If the environment variable (set by Render)
+    # is present, use this to open the connection.
+    if ENV['DATABASE_URL'] != nil
+      @connection = PG.connect(ENV['DATABASE_URL'])
+      return
+    end
+  
     if ENV['ENV'] == 'test'
       database_name = 'music_library_test'
     else
       database_name = 'music_library'
     end
     @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
-  end
-
-  # This method executes an SQL query 
-  # on the database, providing some optional parameters
-  # (you will learn a bit later about when to provide these parameters).
-  def self.exec_params(query, params)
-    if @connection.nil?
-      raise 'DatabaseConnection.exec_params: Cannot run a SQL query as the connection to'\
-      'the database was never opened. Did you make sure to call first the method '\
-      '`DatabaseConnection.connect` in your app.rb file (or in your tests spec_helper.rb)?'
-    end
-    @connection.exec_params(query, params)
   end
 end
